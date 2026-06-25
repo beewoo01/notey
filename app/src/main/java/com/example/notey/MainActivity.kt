63ec764e6e40f112d762b09543454924475e3372
damon <damon@pincrux.com>
2026-06-25 11:09:08 +0900

    111. Displaying All Records in DB

--- app/src/main/java/com/example/notey/MainActivity.kt
+++ app/src/main/java/com/example/notey/MainActivity.kt
@@ -9,17 +9,38 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
+import com.example.notey.repository.NotesRepository
+import com.example.notey.roomdb.NotesDB
 import com.example.notey.ui.theme.NoteyTheme
+import com.example.notey.viewmodel.NoteViewModel
+import com.example.notey.viewmodel.NoteViewModelFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // Room DB
+        val database = NotesDB.getInstance(applicationContext)
+
+        val repository = NotesRepository(database.notesDao)
+
+        val vieWModelFactory = NoteViewModelFactory(repository)
+
+        val noteViewModel = ViewModelProvider(
+            this,
+            vieWModelFactory
+        )[NoteViewModel::class.java]
+
+
         setContent {
             NoteyTheme {
-
+                val notes by noteViewModel.allNotes.observeAsState(emptyList())
             }
         }
     }
